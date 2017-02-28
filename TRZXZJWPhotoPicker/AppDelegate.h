f3f1960c32b45388075f3a73b91c22054250ff80
zhangjw@mmwipo.com <zhangjw@mmwipo.com>
2017-02-28 14:05:08 +0800

    Initial Commit

---
+++ TRZXZJWPhotoPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZXZJWPhotoPicker
+//
+//  Created by 张江威 on 2017/2/28.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRZXZJWPhotoPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRZXZJWPhotoPicker
+//
+//  Created by 张江威 on 2017/2/28.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
